c1bf2b293913d5a28e7ce90a8acbaaf341375640
Brian Davidson <bsdavidson@gmail.com>
2015-02-16 21:42:12 -0600

    Added seek capability, working on passing that back into requests

--- app.rb
+++ app.rb
@@ -17,19 +17,25 @@ class App < Sinatra::Base
     end
   end
 
-  def log_to_array(filename, string = nil)
+  def log_to_array(filename, string = nil, seek = 0)
     log_array = []
+    last_file_pos = 0
     File.open(File.join(settings.logs,filename), 'r') do |f1|
+      f1.seek(seek)
       while line = f1.gets
+         last_file_pos = f1.pos
         if string.nil? or line.include? string
           log_array << line
         end
       end
+
     end
 
+    log_array = log_array.reverse
     return {
       filename: filename,
-      lines: log_array.reverse
+      lastfilepos: last_file_pos,
+      lines: log_array[0...1000]
     }
   end
 
@@ -54,8 +60,8 @@ class App < Sinatra::Base
   get '/api/v1/logs/:logfile/?' do
     file = params[:logfile]
     filter = params[:filter]
-    output = log_to_array(file, filter)
-    puts output.length
+    seek = params[:seek].to_i
+    output = log_to_array(file, filter, seek)
     output.to_json
   end
 
--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,9 @@ if (!Date.now) {
 var LR = {
   startTime: Math.floor(Date.now() / 1000),
   currentTime: Math.floor(Date.now() / 1000),
+  logArray: [],
   lastLineCount: 0,
+  lastFilePos: 0,
   Models: {},
   Collections: {},
   Views: {},
@@ -34,20 +36,15 @@ LR.Collections.Logs = Backbone.Collection.extend({
 // Collection VIEW
 LR.Templates.logsSelect = _.template($("#tmplt-LogsSelect").html());
 
-/*
-log.fetch({
-  data: {
-    filter: 'xxx'
-  }
-});
-*/
-
 LR.Views.Logs = Backbone.View.extend({
   el: $('.logs-view'),
   template: LR.Templates.logsSelect,
   logTemplate: _.template($("#tmplt-LogLines").html()),
 
-  events: { 'change select': 'changeLog' },
+  events: {
+    'change select': 'changeLog',
+    'keyup #log-search': 'searchChange'
+   },
 
   initialize: function() {
     this.collection.bind('reset', this.render, this);
@@ -69,6 +66,11 @@ LR.Views.Logs = Backbone.View.extend({
     this.updateLog();
   },
 
+  searchChange: function() {
+    LR.lastLineCount = 0;
+    LR.newLineCount = 0;
+  },
+
   updateLog: function() {
     LR.currentTime = Math.floor(Date.now() / 1000);
     timeElaspsed = LR.currentTime - LR.startTime;
@@ -77,33 +79,46 @@ LR.Views.Logs = Backbone.View.extend({
     selectedFile = this.$el.find('select').val();
     searchString = this.$el.find('input').val();
     log = this.collection.get(selectedFile);
+
     log.fetch({
+
       data: {
-        filter: searchString
+        filter: searchString,
+        seek: LR.lastFilePos
       },
+
       success: _.bind(function() {
+
         logArray = log.get('lines');
+        logLen = logArray.length;
+
+        LR.lastFilePos = log.get('lastfilepos');
+
         if (LR.lastLineCount === 0) {
-          LR.lastLineCount = logArray.length;
+          LR.lastLineCount = logLen;
           console.log('Reset Count');
         }
 
-        if (LR.lastLineCount < logArray.length) {
+        if (LR.lastLineCount < logLen) {
+
           if ((LR.currentTime - LR.lastNewLineTime) < 20) {
-            LR.newLineCount = LR.newLineCount + (logArray.length - LR.lastLineCount);
+            LR.newLineCount = LR.newLineCount + (logLen - LR.lastLineCount);
           } else {
-            LR.newLineCount = logArray.length - LR.lastLineCount;
+            LR.newLineCount = logLen - LR.lastLineCount;
           }
 
-          LR.lastLineCount = logArray.length;
+          LR.lastLineCount = logLen;
           console.log('Count Change!', LR.newLineCount);
           LR.lastNewLineTime = LR.currentTime;
         }
+
         this.$el.find('.logblock').html(this.logTemplate({
           log: log
         }));
+
         // console.log(logArray);
-        this.$el.find('#last-line-count').html('# of Matches: ' + logArray.length);
+        this.$el.find('#last-line-count').html('# of Matches: ' + logLen);
+
       }, this)
     });
   }
@@ -118,11 +133,8 @@ LR.Router = Backbone.Router.extend({
 
     defaultRoute: function () {
       console.log("defaultRoute");
-      // LR.logEntries = new LR.Collections.LogEntries();
       LR.logs = new LR.Collections.Logs();
-      // new LR.Views.LogEntries({ collection: LR.logEntries });
       LR.logsView = new LR.Views.Logs({ collection: LR.logs });
-      //LR.logEntries.fetch();
       LR.logs.fetch({
         success: function() {
           console.log(LR.logs.length);
