3cdabe13e6a70d52fd421ad7e56c33db713edaed
Brian Davidson <bsdavidson@gmail.com>
2016-03-15 15:06:54 -0700

    Using saner function to traverse dom in highlight function.

--- public/vendor/jquery.highlight-5.js
+++ public/vendor/jquery.highlight-5.js
@@ -18,9 +18,10 @@ jQuery.fn.highlight = function(pat) {
  function innerHighlight(node, pat) {
   var skip = 0;
   if (node.nodeType == 3) {
-   var pos = node.data.toUpperCase().indexOf(pat);
-   pos -= (node.data.substr(0, pos).toUpperCase().length - node.data.substr(0, pos).length);
-   if (pos >= 0) {
+    var pos = node.data.toUpperCase().indexOf(pat);
+    pos -= (node.data.substr(0, pos).toUpperCase().length -
+            node.data.substr(0, pos).length);
+  if (pos >= 0) {
     var spannode = document.createElement('span');
     spannode.className = 'highlight';
     var middlebit = node.splitText(pos);
@@ -30,16 +31,17 @@ jQuery.fn.highlight = function(pat) {
     middlebit.parentNode.replaceChild(spannode, middlebit);
     skip = 1;
     // Added to for unhiding Table row
-    var parentTR = node.parentNode.parentNode.parentNode;
+    var parentTR = $(node).closest('tr');
     $(parentTR).addClass('unhide');
     // console.log(parent);
    }
   }
-  else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
-   for (var i = 0; i < node.childNodes.length; ++i) {
-    i += innerHighlight(node.childNodes[i], pat);
-   }
-  }
+  else if (node.nodeType == 1 && node.childNodes &&
+    !/(script|style)/i.test(node.tagName)) {
+      for (var i = 0; i < node.childNodes.length; ++i) {
+        i += innerHighlight(node.childNodes[i], pat);
+      }
+    }
   return skip;
  }
  return this.length && pat && pat.length ? this.each(function() {
@@ -51,7 +53,7 @@ jQuery.fn.removeHighlight = function() {
  return this.find("span.highlight").each(function() {
   this.parentNode.firstChild.nodeName;
   // Added to remove table row class for hiding
-  var parentTR = this.parentNode.parentNode.parentNode;
+  var parentTR = $(node).closest('tr');
   parentTR.className = '';
   with (this.parentNode) {
    replaceChild(this.firstChild, this);
